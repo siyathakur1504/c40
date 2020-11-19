16a7b174b1c5596340a58de847aea83c80bdc743
Siddhartha Thakur <siddharthathakur@Siyas-MacBook-Air.local>
2020-11-19 18:34:04 +0530

    added the rank

--- js/Game.js
+++ js/Game.js
@@ -44,6 +44,8 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
+
+    player.getCarsAtEnd();
     
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
@@ -93,6 +95,8 @@ class Game {
 
     if(player.distance > 3860){
       gameState = 2;
+      player.rank+=1;
+      Player.updatecarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -100,5 +104,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
